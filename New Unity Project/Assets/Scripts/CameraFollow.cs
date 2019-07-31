874f4d25aa09389be8cf085985d1028697e42036
Lee10 <leembuyisa22@gmail.com>
2019-07-31 20:24:09 +0200

    This is the updated unity scene with the obstacles moving down. decided to place instead of randomly spawn them

---
+++ New Unity Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+
+//camera follow to be attached to the camera 
+  //keep the offest at 0 or 4 because it the view of the player that is the target 
+  {
+	public float yOffset;
+    private Vector3 initialPosition;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this will the reference to the player as it is the target position; disabled for now 
+        //target = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = initialPosition + Vector3.up * yOffset + target.position;
+    }
+}
+
+
---
+++ New Unity Project/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+
+  //this will be attached to the obstacles
+
+    public float ObstacleSpeed = 2f;
+    private float ObstOnScreen;
+   
+    void Start()
+    {
+      
+       ObstOnScreen = -Camera.main.orthographicSize * transform.localScale.y;
+    }
+
+    void Update()
+    {
+        transform.Translate(0, -ObstacleSpeed * Time.deltaTime, 0);
+    }
+    void FixedUpdate()
+    {
+        
+        if (transform.position.y < ObstOnScreen)
+        {
+            Destroy(gameObject);
+
+        }
+    }
+ 
+
+}
