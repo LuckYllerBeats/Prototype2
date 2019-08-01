53364b0c94b13e0594ef8b609e21f3ad8a87769d
kamotselane <kamogelotselane@gmail.com>
2019-08-01 17:32:01 +0200

    Added player's movement rotation and edited the structure of the player in the hierarchy. Main Level.

---
+++ New Unity Project/Assets/Scripts/playerRotationMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerRotationMovement : MonoBehaviour {
+
+    public KeyCode left;
+    public KeyCode right;
+    public float maxTimer = 0.5f;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+        if (Input.GetKey(left))
+        {
+            StartCoroutine(RotateObjectLeft());
+        }
+
+        else if (Input.GetKey(right))
+        {
+            StartCoroutine(RotateObjectRight());
+        }
+    }
+    IEnumerator RotateObjectLeft()
+    {
+        float timer = 0f;
+        while (timer <= maxTimer)
+        {
+            transform.Rotate(new Vector3(0, 0, 60) * Time.deltaTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator RotateObjectRight()
+    {
+        float timer = 0f;
+        while (timer <= maxTimer)
+        {
+            transform.Rotate(new Vector3(0, 0, -60) * Time.deltaTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
