ddeed7489699fc81c8b4b247c8bf61407750bae4
LuckYllerBeats <36919645+LuckYllerBeats@users.noreply.github.com>
2019-08-02 10:49:06 +0200

    Added Visual FX

---
+++ New Unity Project/Assets/FX/SoundTester.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //FindObjectOfType<AudioManager>().Play("BG Sound");    //Play bounce sound
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            FindObjectOfType<AudioManager>().Play("BG Sound");    //BG Sound
+        }
+
+    }
+
+   
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(.5f);
+        
+    }
+}
---
+++ New Unity Project/Assets/FXTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXTest : MonoBehaviour {
+
+    public GameObject bloodFX;
+    public GameObject []bloodTrailFX;
+ 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            //Play Sound & INstantiate particles
+            Instantiate(bloodFX, transform.position, Quaternion.identity);      //Instantiate bloodFX from the position of contact
+           // Instantiate(bloodTrailFX, new Vector3(0, -2, 0), Quaternion.identity); //Instantiate bloodFX (Transform.pos or Vector?)
+            print("Ready to play sound & FX");
+        }
+    }
+}
