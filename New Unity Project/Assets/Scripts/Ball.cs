a50c883e113480e0191ca1ba91e54c7da06ffe85
Lee10 <leembuyisa22@gmail.com>
2019-08-02 09:24:16 +0200

    I made changes to the main scene and menu scene

---
+++ New Unity Project/Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour {
+
+
+    public float Speed = 150f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0f, 0f, Speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0f, 0f, -Speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+            //FindObjectOfType<CameraShake>().StartCoroutine(FindObjectOfType<CameraShake>().ShakeCamera(0.2f, 0.3f));
+
+        }
+    }
+}
