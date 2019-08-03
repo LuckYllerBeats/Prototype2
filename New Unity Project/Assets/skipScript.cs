7f3268d4df795d0fc78284bc459e174856d73a23
kamotselane <kamogelotselane@gmail.com>
2019-08-04 00:58:55 +0200

    Tutorial scene developed further

---
+++ New Unity Project/Assets/skipScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class skipScript : MonoBehaviour {
+
+    private bool isClicked = false;
+
+    private void OnMouseDown()
+    {
+        isClicked = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isClicked = false;
+    }
+    void Update()
+    {
+        if (isClicked)
+
+        {
+
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
