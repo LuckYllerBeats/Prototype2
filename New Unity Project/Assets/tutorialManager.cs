50e0388f1b385132ee408b9a7f555b4b0a79617c
kamotselane <kamogelotselane@gmail.com>
2019-08-04 09:36:19 +0200

    Changed the way the tutorial happens - it is now solely an animation

--- New Unity Project/Assets/tutorialManager.cs
+++ New Unity Project/Assets/tutorialManager.cs
@@ -5,40 +5,22 @@ using UnityEngine;
 public class tutorialManager : MonoBehaviour {
 
     public GameObject obone, obtwo, obthree, obfour, rules;
-    public KeyCode left, right;
+
 
 
 	// Use this for initialization
 	void Start () {
-        obone.gameObject.SetActive(false);
-        obtwo.gameObject.SetActive(false);
-        obthree.gameObject.SetActive(false);
-        obfour.gameObject.SetActive(false);
+        obone.gameObject.SetActive(true);
+        obtwo.gameObject.SetActive(true);
+        obthree.gameObject.SetActive(true);
+        obfour.gameObject.SetActive(true);
         rules.gameObject.SetActive(true);
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
-        if (Input.GetKey(left))
-        {
-            obone.gameObject.SetActive(true);
-            obtwo.gameObject.SetActive(true);
-            obthree.gameObject.SetActive(true);
-            obfour.gameObject.SetActive(true);
-            rules.gameObject.SetActive(false);
-
-        }
-
-        if (Input.GetKey(right))
-        {
-            obone.gameObject.SetActive(true);
-            obtwo.gameObject.SetActive(true);
-            obthree.gameObject.SetActive(true);
-            obfour.gameObject.SetActive(true);
-            rules.gameObject.SetActive(false);
-
-        }
+        
 
     }
 }
